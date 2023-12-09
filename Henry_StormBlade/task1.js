97da228f0f53cb6958a15c06e223b652edaa1874
igor <igor932010@mail.ru>
2023-12-10 01:39:27 +0300

    task 1.2

---
+++ Henry_StormBlade/task1.js
@@ -0,0 +1,27 @@
+const user = {
+  name: 'Henry StormBlade',
+  image: 'Henry_StormBlade.png',
+  corsair: true,
+  legend:
+    "Генри Штормовой клинок - высокий и мускулистый пират. Храбрый и решительный. Служил на 'Чёрной Жемчужине'",
+  friends: ['Alex Hook', 'cap'],
+  coins: [],
+  ship: (7 + 6) % 2,
+  location: [],
+  github: '',
+};
+
+function getCoin() {
+  return Math.floor(Math.random() * 16 ** 5).toString(16);
+}
+for (let i = 0; i < 3; i += 1) {
+  user.coins.push(getCoin());
+}
+
+function getCoord() {
+  return Math.floor(Math.random() * 100);
+}
+
+user.location = [getCoord(), getCoord()];
+user.github = 'garryKarri';
+console.log(user);
