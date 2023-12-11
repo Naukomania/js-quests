d4f93bfd8f5034502b74f2ea24db9f05612a4959
naukomania <naukomania@gmail.com>
2023-12-11 08:41:36 +0300

    task 3 sample

---
+++ cap/random.js
@@ -0,0 +1,11 @@
+const getRandomColor = () => {};
+const getDice = () => {};
+const getRPS = () => {};
+const getPhrase = () => {};
+
+module.exports = {
+  getRandomColor,
+  getDice,
+  getRPS,
+  getPhrase,
+};
