83c3655fd3a5d7e6ee3875d7ef7150614421ba69
Nadezhda Dorogi <Nadezhda_dorgi@mail.ru>
2023-12-14 20:31:28 +0300

    task1

--- esperanca/task1.js
+++ esperanca/task1.js
@@ -9,3 +9,30 @@ const userSchema = {
   location: [Number],
   github: String,
 };
+const user = {
+  name: 'Esperanca',
+  image: 'esperanca.png',
+  corsair: false,
+  legend: 'Путешественница. Видит будущее. Там, где она всегда есть Надежда.',
+  friends: ['Cap', 'Maria', 'Structia', 'HellsingLan'],
+  coins: [],
+  ship: ((30 + 8) % 2) + 1,
+  location: [],
+};
+
+function getCoin() {
+  return Math.floor(Math.random() * 16 ** 5).toString(16);
+}
+
+function getCoord() {
+  return Math.floor(Math.random() * 100);
+}
+
+for (let i = 0; i < 3; i += 1) {
+  user.coins.push(getCoin());
+}
+
+user.location = [getCoord(), getCoord()];
+user.github = 'NadezhdaDorogi';
+
+console.log(JSON.stringify(user, null, 2));
