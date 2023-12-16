3f9cc2bb2f333a0ad0e0afe7670234b07845efac
Nikita Tikhomirov <Q89517286969@yandex.ru>
2023-12-16 14:00:05 +0300

    Quest_1.4 finall

--- hellsinglan/pirate.js
+++ hellsinglan/pirate.js
@@ -4,11 +4,23 @@ function sayHi(user) {
   console.log(`Йо‑хо‑хо! Меня зовут ${user.name}. Я - не корсар.`);
 }
 
-function moveTo() {
-    
+function moveTo(user, moveX, moveY) {
+  if (-1 <= moveX <= 1 && -1 <= moveY <= 1) {
+    if (0 <= user.location[0] <= 100) user.location[0] += moveX;
+    if (0 <= user.location[1] <= 100) user.location[1] += moveY;
+  }
+  return user.location;
 }
 
 module.exports = {
   sayHi,
-  moveTo
+  moveTo,
 };
+
+// function moveTo (y) {
+//   let x = 10;
+//   if (0 <= y < 100) x += y;
+//   console.log(x);
+// }
+
+// moveTo(4);
