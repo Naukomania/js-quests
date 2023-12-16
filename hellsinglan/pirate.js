6fd8f1f462f26aff39861335d6fde54b8070e152
Nikita Tikhomirov <Q89517286969@yandex.ru>
2023-12-16 12:52:51 +0300

    Quest_1.4 (not realization moveTo)

---
+++ hellsinglan/pirate.js
@@ -0,0 +1,14 @@
+// const user = require('./profile.json');
+
+function sayHi(user) {
+  console.log(`Йо‑хо‑хо! Меня зовут ${user.name}. Я - не корсар.`);
+}
+
+function moveTo() {
+    
+}
+
+module.exports = {
+  sayHi,
+  moveTo
+};
