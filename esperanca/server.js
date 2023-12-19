611533d9ffbb86d1b0130a5e9ba199d18e14a7e2
Nadezhda Dorogi <Nadezhda_dorgi@mail.ru>
2023-12-19 22:31:25 +0300

    task2.2

---
+++ esperanca/server.js
@@ -0,0 +1,9 @@
+const app = require('../app');
+const profiles = require('../profiles.json');
+app.get('/', function (req, res) {
+    res.json('Hello World!');
+  });
+  
+  app.get('/profiles', function (req, res) {
+    res.json(profiles);
+  });
