a2be57e95a2a741b3d9634a6ccd81cc7a216696b
mksmvnv <mksmvnv@icloud.com>
2023-12-23 11:39:25 +0300

    Add task 2.4

--- structia/server.js
+++ structia/server.js
@@ -2,6 +2,7 @@ const app = require('../app');
 const profiles = require('../profiles.json');
 const random = require('./random');
 const lib = require('./lib');
+const pirate = require('./pirate');
 
 app.get('/', (req, res) => {
   res.json('Hello World!');
@@ -63,4 +64,39 @@ app.get('/random/coords', (req, res) => {
 
 app.get('/random/coins', (req, res) => {
   res.json(lib.getCoins(3));
+});
+
+app.get('/profiles/me', (req, res) => {
+  const structia = profiles.find((profile) => profile.name === 'Structia');
+  for (let i = 0; i < structia.friends.length; i++) {
+    const friend_name = structia.friends[i];
+    const friend_profile = profiles.find(
+      (profile) => profile.name === friend_name
+    );
+    if (friend_profile) {
+      const { name, image, legend, ship } = friend_profile;
+      structia.friends[i] = { name, image, legend, ship };
+    } else {
+      structia.friends.splice(i, 1);
+      i--;
+    }
+  }
+  const { name, image, legend, friends, ship } = structia;
+  res.json({ name, image, legend, friends, ship });
+});
+
+app.get('/profiles/me/coords', (req, res) => {
+  const structia = profiles.find((profile) => profile.name === 'Structia');
+  res.json(structia.location);
+});
+
+app.post('/profiles/me/move', (req, res) => {
+  const structia = profiles.find((profile) => profile.name === 'Structia');
+  const x = req.body.x;
+  const y = req.body.y;
+  if (x && y) {
+    res.json(pirate.moveTo(structia, x, y));
+  } else {
+    res.json('Перекати поле ..*..');
+  }
 });
