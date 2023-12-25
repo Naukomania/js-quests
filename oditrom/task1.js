48b6e10b7a86858c2ca24fd9ede45e92b4005bd9
Oditrom <sergedoc@yandex.ru>
2023-12-25 16:48:57 +0300

    task1&2

---
+++ oditrom/task1.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "Oditrom",
+  image: "oditrom.png",
+  corsair: true,
+  legend:
+    "Матерый морской волк. Знает, что если дело важное, то не надо суетиться.",
+  friends: ["Bloomberg", "AlexHook", "Maria"],
+  github: "Oditrom",
+};
+console.log(JSON.stringify(user,null,2));
