cdf8a414ae33a1a470ad1c52ba331390a9d3ed58
Nikita Tikhomirov <Q89517286969@yandex.ru>
2023-12-29 15:55:58 +0300

    quest_3.2_2

--- hellsinglan/bot.js
+++ hellsinglan/bot.js
@@ -20,44 +20,96 @@ const bot = require('../bot');
 
 // 1
 
-const mapSize = 100;
-let piratePosition = { x: 0, y: 0 };
+// const mapSize = 100;
+// let piratePosition = { x: 0, y: 0 };
 
-// Обработка команды "/start"
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Привет, пират! Начнем поиски сокровищ!");
-  piratePosition = { x: 50, y: 50 }; // Стартовая позиция пирата
-});
+// // Обработка команды "/start"
+// bot.onText(/\/start/, (msg) => {
+//   bot.sendMessage(msg.chat.id, "Привет, пират! Начнем поиски сокровищ!");
+//   piratePosition = { x: 50, y: 50 }; // Стартовая позиция пирата
+// });
 
-// Обработка команд перемещения
-bot.onText(/\/move (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const direction = match[1];
+// // Обработка команд перемещения
+// bot.onText(/\/move (.+)/, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const direction = match[1];
 
-  // Перемещение пирата
-  switch (direction) {
-    case 'вверх':
-      piratePosition.y = Math.max(0, piratePosition.y - 1);
-      break;
-    case 'вниз':
-      piratePosition.y = Math.min(mapSize - 1, piratePosition.y + 1);
-      break;
-    case 'влево':
-      piratePosition.x = Math.max(0, piratePosition.x - 1);
-      break;
-    case 'вправо':
-      piratePosition.x = Math.min(mapSize - 1, piratePosition.x + 1);
-      break;
-    default:
-      bot.sendMessage(chatId, "Я не знаю такого направления!");
-      return;
-  }
+//   // Перемещение пирата
+//   switch (direction) {
+//     case 'вверх':
+//       piratePosition.y = Math.max(0, piratePosition.y - 1);
+//       break;
+//     case 'вниз':
+//       piratePosition.y = Math.min(mapSize - 1, piratePosition.y + 1);
+//       break;
+//     case 'влево':
+//       piratePosition.x = Math.max(0, piratePosition.x - 1);
+//       break;
+//     case 'вправо':
+//       piratePosition.x = Math.min(mapSize - 1, piratePosition.x + 1);
+//       break;
+//     default:
+//       bot.sendMessage(chatId, "Я не знаю такого направления!");
+//       return;
+//   }
 
-  bot.sendMessage(chatId, `Пират переместился на [${piratePosition.x}, ${piratePosition.y}]`);
-});
+//   bot.sendMessage(chatId, `Пират переместился на [${piratePosition.x}, ${piratePosition.y}]`);
+// });
 
-// Запуск бота
-bot.on('polling_error', (error) => console.log(error));
+// // Запуск бота
+// bot.on('polling_error', (error) => console.log(error));
+
+// 2
+
+// const mapSize = 100;
+// let piratePosition = { x: 50, y: 50 };
+// let enemies = [{ x: randomCoordinate(), y: randomCoordinate() }];
+// let treasure = { x: randomCoordinate(), y: randomCoordinate() };
+
+// function randomCoordinate() {
+//   return Math.floor(Math.random() * mapSize);
+// }
+
+// bot.onText(/\/start/, (msg) => {
+//   bot.sendMessage(msg.chat.id, "Привет, пират! Начнем поиски сокровищ!");
+//   piratePosition = { x: 50, y: 50 }; // Стартовая позиция пирата
+//   enemies = [{ x: randomCoordinate(), y: randomCoordinate() }]; // Позиция врага
+//   treasure = { x: randomCoordinate(), y: randomCoordinate() }; // Позиция сокровища
+// });
+
+// bot.onText(/\/move (.+)/, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const direction = match[1];
+
+//   switch (direction) {
+//     case 'вверх':
+//       piratePosition.y = Math.max(0, piratePosition.y - 1);
+//       break;
+//     case 'вниз':
+//       piratePosition.y = Math.min(mapSize - 1, piratePosition.y + 1);
+//       break;
+//     case 'влево':
+//       piratePosition.x = Math.max(0, piratePosition.x - 1);
+//       break;
+//     case 'вправо':
+//       piratePosition.x = Math.min(mapSize - 1, piratePosition.x + 1);
+//       break;
+//     default:
+//       bot.sendMessage(chatId, "Я не знаю такого направления!");
+//       return;
+//   }
+
+//   if (piratePosition.x === treasure.x && piratePosition.y === treasure.y) {
+//     bot.sendMessage(chatId, "Поздравляю! Ты нашел сокровище!");
+//     treasure = { x: randomCoordinate(), y: randomCoordinate() };
+//   } else if (enemies.some(enemy => enemy.x === piratePosition.x && enemy.y === piratePosition.y)) {
+//     bot.sendMessage(chatId, "О нет! Ты встретил врага!");
+//   } else {
+//     bot.sendMessage(chatId, `Пират переместился на [${piratePosition.x}, ${piratePosition.y}]`);
+//   }
+// });
+
+// bot.on('polling_error', (error) => console.log(error));
 
 // 3
 
