d5aa06aef72607be0f80b398a9e52ebf19d09425
Nadezhda Dorogi <Nadezhda_dorgi@mail.ru>
2023-12-29 12:22:03 +0300

    задание 3.1

---
+++ esperanca/bot.js
@@ -0,0 +1,18 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, '../.env'),
+});
+
+const bot = require('../bot');
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+bot.on('message', (msg) => {
+  console.log({ msg });
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
