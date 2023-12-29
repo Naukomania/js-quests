ce5105db877cfb652d152b488699ec8832ccba51
Nikita Tikhomirov <Q89517286969@yandex.ru>
2023-12-29 15:52:51 +0300

    quest_3.1_1

--- hellsinglan/bot.js
+++ hellsinglan/bot.js
@@ -4,19 +4,62 @@ require('dotenv').config({
 });
 const bot = require('../bot');
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const resp = match[1];
 
-  bot.sendMessage(chatId, resp);
+//   bot.sendMessage(chatId, resp);
+// });
+
+// bot.on('message', (msg) => {
+//   console.log({ msg });
+//   const chatId = msg.chat.id;
+//   if (msg.text === 'Привет!') bot.sendMessage(chatId, 'Приветище, дорогой!');
+//   else bot.sendMessage(chatId, 'Received your message');
+// });
+
+// 1
+
+const mapSize = 100;
+let piratePosition = { x: 0, y: 0 };
+
+// Обработка команды "/start"
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Привет, пират! Начнем поиски сокровищ!");
+  piratePosition = { x: 50, y: 50 }; // Стартовая позиция пирата
 });
 
-bot.on('message', (msg) => {
-  console.log({ msg });
+// Обработка команд перемещения
+bot.onText(/\/move (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-  if (msg.text === 'Привет!') bot.sendMessage(chatId, 'Приветище, дорогой!');
-  else bot.sendMessage(chatId, 'Received your message');
+  const direction = match[1];
+
+  // Перемещение пирата
+  switch (direction) {
+    case 'вверх':
+      piratePosition.y = Math.max(0, piratePosition.y - 1);
+      break;
+    case 'вниз':
+      piratePosition.y = Math.min(mapSize - 1, piratePosition.y + 1);
+      break;
+    case 'влево':
+      piratePosition.x = Math.max(0, piratePosition.x - 1);
+      break;
+    case 'вправо':
+      piratePosition.x = Math.min(mapSize - 1, piratePosition.x + 1);
+      break;
+    default:
+      bot.sendMessage(chatId, "Я не знаю такого направления!");
+      return;
+  }
+
+  bot.sendMessage(chatId, `Пират переместился на [${piratePosition.x}, ${piratePosition.y}]`);
 });
+
+// Запуск бота
+bot.on('polling_error', (error) => console.log(error));
+
+// 3
 
 // const mapSize = 100;
 // let piratePosition = { x: 50, y: 50 };
